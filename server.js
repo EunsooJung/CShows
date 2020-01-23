786a553abc65c4a17887272e88c17d2f83196419
EunsooJung <hi.michael.jung@gmail.com>
2020-01-23 11:26:40 -0800

    CShows initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+// setup middleware
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// To use local static file
+app.use(express.static(process.cwd() + '/public'));
+
+// setup body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// setup method-override
+app.use(methodOverride('_method'));
+
+// Setup Handlebars engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function() {
+  console.log('Burger Server listening on: http://localhost: ' + PORT);
+});
