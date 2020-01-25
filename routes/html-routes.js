2ebf91e9e0901b888702877bbe179f760a86efe9
skuttenkuler <samkiddmusic@gmail.com>
2020-01-25 14:27:14 -0800

    new routes, htmls, authentication

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,10 +4,32 @@ var path = require("path");
 var isAuthenicated = require("../config/middleware/isAuthenicated");
 
 module.exports = function(app) {
-  path.basename.length("/", function(req, res) {
-    if(req.user) {
-      res.redirect("/users");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"))
-  })
+ app.get("/", function(req,res) {
+   if(req.user) {
+     res.redirect("/dashboard");
+   }
+   res.sendfile(path.join(__dirname, "../public/assets/welcome.html"))
+ });
+ app.get("/login", function(req,res) {
+  if(req.user) {
+    res.redirect("/dashboard");
+  }
+  res.sendfile(path.join(__dirname, "../public/assets/login.html"))
+});
+app.get("/signup", function(req,res) {
+  if(req.user) {
+    res.redirect("/dashboard");
+  }
+  res.sendfile(path.join(__dirname, "../public/assets/signup.html"))
+});
+app.get("/artistsignup", function(req,res) {
+  if(req.user) {
+    res.redirect("/dashboard");
+  }
+  res.sendfile(path.join(__dirname, "../public/assets/signupArtist.html"))
+});
+app.get("/dashboard", isAuthenicated, function(req,res) {
+  res.sendFile(path.join(__dirname, "../public/assets/dashboard.html"))
+})
+
 }
