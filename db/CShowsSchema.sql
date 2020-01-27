746ef0a52fcfdedc0179b44c65a9ca38ced2cc2e
Tai Le <tle449@gmail.com>
2020-01-27 11:20:32 -0800

    changing schema.sql but it didnt work

--- db/CShowsSchema.sql
+++ db/CShowsSchema.sql
@@ -7,7 +7,6 @@ CREATE TABLE users (
   user_name VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  user_type VARCHAR(100) NOT NULL,
   location VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
@@ -15,37 +14,34 @@ CREATE TABLE users (
 CREATE TABLE tickets
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	ticket_name varchar(255) NOT NULL,
-	ticket_type varchar(255) NOT NULL,
-	ticket_cost int NOT NULL,
 	PRIMARY KEY (id),
 	index(id),
-	index(ticket_name),
-	FOREIGN KEY (id) REFERENCES users(id)
+	FOREIGN KEY (id) REFERENCES users(id),
+	FOREIGN KEY (artist_name) REFERENCES artists(artist_name),
+	FOREIGN KEY (genre) REFERENCES artists(genre),
+	FOREIGN KEY (show_venue) REFERENCES shows(show_venue)
 );
 
 CREATE TABLE artists
 (
 	id int NOT NULL AUTO_INCREMENT,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	artist_name varchar(255) NOT NULL,
-	artist_type varchar(255) NOT NULL,
-  index(id),
-  index(name),
+	genre varchar(225) NOT NULL,
+	index(id),
+	index(artist_name),
 	PRIMARY KEY (id)
-	-- FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE shows
 (
 	id int NOT NULL AUTO_INCREMENT,
-	show_name varchar(255) NOT NULL,
-	show_type varchar(255) NOT NULL,
 	show_venue varchar(255) NOT NULL,
 	show_Date DATE NOT NULL,
-	show_genre varchar(255) NOT NULL,
 	PRIMARY KEY (id),
 	index(id),
-	index(show_name),
-	FOREIGN KEY (id) REFERENCES tickets(id),
-	FOREIGN KEY (id) REFERENCES artists(id)
+	index(artist_name),
+	FOREIGN KEY (artist_name) REFERENCES artists(artist_name),
+	FOREIGN KEY (genre) REFERENCES artists(genre)
 );
