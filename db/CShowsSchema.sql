d1c17f00e64e5658b86c030ad60c3065dcabe556
skuttenkuler <samkiddmusic@gmail.com>
2020-01-27 13:54:29 -0800

    new sql

--- db/CShowsSchema.sql
+++ db/CShowsSchema.sql
@@ -7,45 +7,40 @@ CREATE TABLE users (
   user_name VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  user_type VARCHAR(100) NOT NULL,
   location VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
-
-CREATE TABLE tickets
-(
-	id INT NOT NULL AUTO_INCREMENT,
-	ticket_name varchar(255) NOT NULL,
-	ticket_type varchar(255) NOT NULL,
-	ticket_cost int NOT NULL,
-	PRIMARY KEY (id),
-	index(id),
-	index(ticket_name),
-	FOREIGN KEY (id) REFERENCES users(id)
-);
-
 CREATE TABLE artists
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	artist_name varchar(255) NOT NULL,
-	artist_type varchar(255) NOT NULL,
-  index(id),
-  index(name),
+	genre varchar(225) NOT NULL,
 	PRIMARY KEY (id)
-	-- FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE shows
 (
 	id int NOT NULL AUTO_INCREMENT,
-	show_name varchar(255) NOT NULL,
-	show_type varchar(255) NOT NULL,
 	show_venue varchar(255) NOT NULL,
+    artist_name INT,
+    user_id INT,
 	show_Date DATE NOT NULL,
-	show_genre varchar(255) NOT NULL,
-	PRIMARY KEY (id),
-	index(id),
-	index(show_name),
-	FOREIGN KEY (id) REFERENCES tickets(id),
-	FOREIGN KEY (id) REFERENCES artists(id)
+    FOREIGN KEY (artist_name) REFERENCES artists(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+	PRIMARY KEY (id)
 );
+
+
+CREATE TABLE tickets
+(
+	id INT NOT NULL AUTO_INCREMENT,
+    artist_name INT,
+    show_id INT NOT NULL,
+	PRIMARY KEY (id),
+
+    FOREIGN KEY (artist_name) REFERENCES artists(id),
+	FOREIGN KEY (show_id) REFERENCES shows(id)
+);
+
