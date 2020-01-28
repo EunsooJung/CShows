7a41e576f6c40d5fb5fe09e966a189f2945709e1
skuttenkuler <samkiddmusic@gmail.com>
2020-01-28 12:04:31 -0800

    artist model, ticket and artist handlebars column

---
+++ controllers/artistController.js
@@ -0,0 +1,44 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var Artist = require("../models/artist.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  Artist.all(function(data) {
+    var hbsObject = {
+      artists: data
+    };
+    console.log(hbsObject);
+    res.render("artists", hbsObject);
+  });
+});
+
+router.post("/api/artists", function(req, res) {
+  Artist.create([
+    "email", "password", "artist_name", "genre"
+  ], [
+    req.body.email, req.body.password, req.body.artist_name, req.body.genre
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.delete("/api/artists/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  Artist.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
